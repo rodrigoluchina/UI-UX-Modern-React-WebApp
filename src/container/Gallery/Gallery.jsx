420efe91673a1ad9a944b9f620fabf1d89e4df2b
rodrigoluchina <rodrigolucchina@hotmail.com>
2023-01-14 14:42:56 -0300

    update in gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -24,7 +24,7 @@ const Gallery = () => {
       <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
         <p className="p__opensans" style={{color:'#AAA',marginTop:'2rem'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button type="button" className="custom__buttom">View More</button>
+        <button type="button" className="custom__button">View More</button>
     </div>
     <div className="app__gallery-images">
       <div className="app__gallery-images_container" ref={scrollRef}>
